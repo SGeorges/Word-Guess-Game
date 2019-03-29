6780ec0626c5dbf4ebfe2add827a846529d8f4e3
SGeorges <SGeorges@github.com>
2019-03-28 21:42:24 -0400

    Final for submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,28 @@
 
 var acceptableGuesses = "abcdefghijklmnopqrstuvwxyz";
-var numGuesses = 0;
+var missGuess = 0;
 var index = 0;
+
+var lockGame = false;
 
 var counterText = document.getElementById("counterText");
 var solverText = document.getElementById("solverText");
 var wrongGuessesText = document.getElementById("wrongGuessesText");
 var resultText = document.getElementById("resultText");
+var winsText = document.getElementById("winsText");
+var endGame = document.getElementById("endGame");
 
 var audioElement = document.createElement("audio");
+audioElement.volume = 0.05;
 
 var solutions = [
     "starlight",
     "showman",
     "refugee",
     "bright",
-    "antithesis"
+    "romance",
+    "black",
+    "caravan"
 ]
 var guesses = []
 
@@ -74,84 +81,102 @@ function writer( x, word ) {
 }
 
 writer(guesses, solutions[index]);
-counterText.textContent = 15 - numGuesses;
+counterText.textContent = 6 - missGuess;
 
 document.onkeyup = function(event) {
     console.log("---You Are Guessing---")
     var userGuess = event.key;
     var word = solutions[index];
 
-    if (numGuesses === 0) {
-        var h = writer(guesses, word);
+    if (lockGame != true) {
+        
+        // Checks if the userGuess is a letter. Numbers/characters don't count.
+        if (acceptableGuesses.indexOf(userGuess) != -1) {
 
-        console.log(h);
+            // Checks if the userGuess has already been guessed
+            if (guessChecker(userGuess, guesses)) {
+                guesses.push(userGuess);
+                h = writer(guesses, word);
+                console.log(h);
 
-    }
-    // Checks if the userGuess is a letter. Numbers/characters don't count.
-    if (acceptableGuesses.indexOf(userGuess) != -1) {
-
-        // Checks if the userGuess has already been guessed
-        if (guessChecker(userGuess, guesses)) {
-            guesses.push(userGuess);
-            h = writer(guesses, word);
-            console.log(h);
-            numGuesses++;
-        }
-
-        counterText.textContent = 15 - numGuesses;
-        wrongGuessesText.textContent = guesses;
-        console.log(numGuesses);
-        console.log(guesses);
-
-        // Checks if the user has entered all of the letters in the word 
-        if (solChecker(guesses, word)) {
-            guesses = [];
-            numGuesses = 0;
-            index++;
-
-            writer(guesses, solutions[index]);
-
-            counterText.textContent = 15 - numGuesses;
-            wrongGuessesText.textContent = guesses;
-            resultText.textContent= "Congratz! The word was " + word;
-
-            switch (index) {
-                case 1: 
-                audioElement.setAttribute("src", "assets/images/MUSE - Starlight.mp3");
-                audioElement.play();
-                break;
-
-                case 2:
-                audioElement.setAttribute("src", "assets/images/Panic! At The Disco - The Greatest Show (from The Greatest Showman Reimagined).mp3");
-                audioElement.play();
-                break;
-
-                case 3:
-                audioElement.setAttribute("src", "assets/images/Rise Against - Prayer Of The Refugee.mp3");
-                audioElement.play();
-                break;
-
-                case 4:
-                audioElement.setAttribute("src", "assets/images/The Killers - Mr. Brightside.mp3");
-                audioElement.play();
-                break;
-                
-
-                default:
-                break;
+                // if the user guess isn't in the word don't count miss guess
+                if (word.indexOf(userGuess) === -1) {
+                    missGuess++;
+                }
             }
 
+            counterText.textContent = 6 - missGuess;
+            wrongGuessesText.textContent = guesses;
 
+            // Checks if the user has entered all of the letters in the word 
+            if (solChecker(guesses, word)) {
+                guesses = [];
+                missGuess = 0;
+                index++;
+
+                if (index != solutions.length){
+                    writer(guesses, solutions[index]);
+                }
+
+                counterText.textContent = 6 - missGuess;
+                wrongGuessesText.textContent = guesses;
+                winsText.textContent = index;
+                resultText.textContent= "Congratulations! The word was '" + word + "'";
+
+                switch (index) {
+                    case 1: 
+                    audioElement.setAttribute("src", "assets/images/MUSE - Starlight.mp3");
+                    audioElement.play();
+                    break;
+
+                    case 2:
+                    audioElement.setAttribute("src", "assets/images/Panic! At The Disco - The Greatest Show (from The Greatest Showman Reimagined).mp3");
+                    audioElement.play();
+                    break;
+
+                    case 3:
+                    audioElement.setAttribute("src", "assets/images/Rise Against - Prayer Of The Refugee.mp3");
+                    audioElement.play();
+                    break;
+
+                    case 4:
+                    audioElement.setAttribute("src", "assets/images/The Killers - Mr. Brightside.mp3");
+                    audioElement.play();
+                    break;
+                    
+                    case 5:
+                    audioElement.setAttribute("src", "assets/images/My Chemical Romance - Helena.mp3");
+                    audioElement.play();
+                    break;
+
+                    case 6:
+                    audioElement.setAttribute("src", "assets/images/The Rolling Stones - Paint It, Black.mp3");
+                    audioElement.play();
+                    break;
+
+                    case 7:
+                    audioElement.setAttribute("src", "assets/images/Caravan Palace - Lone Digger.mp3");
+                    audioElement.play();
+                    break;
+
+                    default:
+                    break;
+                }
+
+                if (index === solutions.length) {
+                    lockGame = true;
+                    document.getElementById("endGame").src="assets/images/rickrolled.gif"
+                }
+
+            }
+            // Checks if the user has run out of guesses
+            if ((6-missGuess) === 0) {
+                resultText.textContent= "OH no, you've run out of guesses. The word was " + word;
+                lockGame = true;
+                document.getElementById("endGame").src="assets/images/fuckolly.jpg"
+
+            }        
         }
-        // Checks if the user has run out of guesses
-        if ((15-numGuesses) === 0) {
-            resultText.textContent= "OH no, you've run out of guesses. The word was " + word;
-        }
-    
     }
-
-    // h = writer(guesses, word);
-    // console.log(h);
-
-
 }
+
